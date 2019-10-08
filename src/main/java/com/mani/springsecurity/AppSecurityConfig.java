5c56c35c965e38e94621a89884e43040593fe259
lokendramani <lokendramani@gmail.com>
2019-10-08 13:04:24 +0530

    BCrypt Support Added

--- src/main/java/com/mani/springsecurity/AppSecurityConfig.java
+++ src/main/java/com/mani/springsecurity/AppSecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +34,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationProvider authProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
 }
